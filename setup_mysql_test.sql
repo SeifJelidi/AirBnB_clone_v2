ef388de250dfe293a83646fba26bfca1826e3bc0
Seif Jelidi <seifjelidi3@gmail.com>
2020-08-13 22:11:32 +0000

    task 4

---
+++ setup_mysql_test.sql
@@ -0,0 +1,7 @@
+-- prepares a MySQL server for the project
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+DROP USER IF EXISTS 'hbnb_test'@'localhost';
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
